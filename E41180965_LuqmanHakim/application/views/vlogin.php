67130a0bf2cca88d7346a4ceefc627816b07d9e1
Luqman Hakim <e41180965@student.polije.ac.id>
2020-03-22 23:39:04 +0700

    session complete

--- E41180965_LuqmanHakim/application/views/vlogin.php
+++ E41180965_LuqmanHakim/application/views/vlogin.php
@@ -5,26 +5,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="<?php echo base_url("login/images/icons/favicon.ico")?>"/>
+	<link rel="icon" type="image/png" href="<?php echo base_url("alogin/images/icons/favicon.ico")?>"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/vendor/bootstrap/css/bootstrap.min.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/vendor/bootstrap/css/bootstrap.min.css")?>">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/fonts/font-awesome-4.7.0/css/font-awesome.min.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/fonts/font-awesome-4.7.0/css/font-awesome.min.css")?>">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/fonts/Linearicons-Free-v1.0.0/icon-font.min.css")?>">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/vendor/animate/animate.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/vendor/animate/animate.css")?>">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/vendor/css-hamburgers/hamburgers.min.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/vendor/css-hamburgers/hamburgers.min.css")?>">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/vendor/animsition/css/animsition.min.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/vendor/animsition/css/animsition.min.css")?>">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/vendor/select2/select2.min.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/vendor/select2/select2.min.css")?>">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/vendor/daterangepicker/daterangepicker.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/vendor/daterangepicker/daterangepicker.css")?>">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/css/util.css")?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("login/css/main.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/css/util.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("alogin/css/main.css")?>">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -32,7 +32,7 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<div class="login100-form-title" style="background-image: url(login/images/backgroundlogin.jpg);">
+				<div class="login100-form-title" style="background-image: url(<?php echo base_url("alogin/images/backgroundlogin.jpg")?>);">
 					<span class="login100-form-title-1">
 						Sign In
 					</span>
@@ -77,21 +77,21 @@
 	</div>
 	
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("login/vendor/jquery/jquery-3.2.1.min.js")?>"></script>
+	<script src="<?php echo base_url("alogin/vendor/jquery/jquery-3.2.1.min.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("login/vendor/animsition/js/animsition.min.js")?>"></script>
+	<script src="<?php echo base_url("alogin/vendor/animsition/js/animsition.min.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("login/vendor/bootstrap/js/popper.js")?>"></script>
-	<script src="<?php echo base_url("login/vendor/bootstrap/js/bootstrap.min.js")?>"></script>
+	<script src="<?php echo base_url("alogin/vendor/bootstrap/js/popper.js")?>"></script>
+	<script src="<?php echo base_url("alogin/vendor/bootstrap/js/bootstrap.min.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("login/vendor/select2/select2.min.js")?>"></script>
+	<script src="<?php echo base_url("alogin/vendor/select2/select2.min.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("login/vendor/daterangepicker/moment.min.js")?>"></script>
-	<script src="<?php echo base_url("login/vendor/daterangepicker/daterangepicker.js")?>"></script>
+	<script src="<?php echo base_url("alogin/vendor/daterangepicker/moment.min.js")?>"></script>
+	<script src="<?php echo base_url("alogin/vendor/daterangepicker/daterangepicker.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("login/vendor/countdowntime/countdowntime.js")?>"></script>
+	<script src="<?php echo base_url("alogin/vendor/countdowntime/countdowntime.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("login/js/main.js")?>"></script>
+	<script src="<?php echo base_url("alogin/js/main.js")?>"></script>
 
 </body>
 </html>
